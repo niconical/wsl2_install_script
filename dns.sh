a0b6087c05b26eaf3dc68d37d54c5216a9262ddf
hangcui <767948418@qq.com>
2022-04-30 11:43:24 +0800

    add dns file

---
+++ dns.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+HOSTS_FILE_WIN='/mnt/c/Windows/System32/drivers/etc/hosts'
+inetIp=`ifconfig eth0 | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*  netmask" | cut -f 2 -d " "`
+
+nu=`cat -n ${HOSTS_FILE_WIN} | grep wsl2 | awk '{print $1}'` 
+
+dnsIp=`cat ${HOSTS_FILE_WIN} | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]* wsl #" | cut -f 1 -d " "`
+
+echo "wsl's ip is: ${inetIp}"
+
+# set -x
+set -e
+if [ ${nu} ] 
+then
+        if [ ${inetIp} != ${dnsIp} ] 
+        then
+                echo reset
+                sed -i "${nu}d" ${HOSTS_FILE_WIN} 
+                echo "${inetIp} wsl #wsl2 dns config" >> ${HOSTS_FILE_WIN}
+        fi
+else
+        echo "${inetIp} wsl #wsl2 dns config" >> ${HOSTS_FILE_WIN}
+fi
--- init.sh
+++ init.sh
@@ -3,10 +3,6 @@ sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo cp ./aliyun_focal_sources.list  /etc/apt
 sudo mv /etc/apt/aliyun_focal_sources.list /etc/apt/sources.list
 
-# wsl2 nameserver tmp
-sudo echo 'nameserver:8.8.8.8' >> /etc/reslov.conf
-sudo echo 'nameserver:8.8.4.4' >> /etc/reslov.conf
-
 # apt
 sudo apt update
 sudo apt upgrade -y
@@ -33,4 +29,15 @@ echo 'export https_proxy=http://$host_ip:7890' >> ~/.bashrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo 'prompt_context(){}' >> ~/.zshrc
+echo 'host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")' >> ~/.zshrc
+echo 'echo %host_ip' >> ~/.zshrc
+echo 'export ALL_PROXY="http://$host_ip:7890"' >> ~/.zshrc
+echo 'export all_proxy="http://$host_ip:7890"' >> ~/.zshrc
+echo 'curl -I https://www.google.com' >> ~/.zshrc
 source ~/.zshrc
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo 'source $HOME/.cargo/env' >> ~/.zshrc
+
+# ref: https://yangchnet.github.io/Dessert/posts/env/wsl2-%E4%B8%8D%E8%BE%93mac%E7%9A%84%E5%BC%80%E5%8F%91%E4%BD%93%E9%AA%8C%E4%BA%8Cwsl2%E7%9A%84%E7%BD%91%E7%BB%9C%E9%97%AE%E9%A2%98/
