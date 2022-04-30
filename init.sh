7f9048a17efb1fc222217b62fd8cbb1533f19092
hangcui <767948418@qq.com>
2022-04-30 11:03:08 +0800

    github cli & sudo

--- init.sh
+++ init.sh
@@ -1,28 +1,33 @@
 # source list
-mv /etc/apt/sources.list /etc/apt/sources.list.bak
-cp ./aliyun_focal_sources.list  /etc/apt
-mv /etc/apt/aliyun_focal_sources.list /etc/apt/sources.list
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo cp ./aliyun_focal_sources.list  /etc/apt
+sudo mv /etc/apt/aliyun_focal_sources.list /etc/apt/sources.list
 
 # wsl2 nameserver tmp
-echo 'nameserver:8.8.8.8' >> /etc/reslov.conf
-echo 'nameserver:8.8.4.4' >> /etc/reslov.conf
+sudo echo 'nameserver:8.8.8.8' >> /etc/reslov.conf
+sudo echo 'nameserver:8.8.4.4' >> /etc/reslov.conf
 
 # apt
-apt update
-apt upgrade -y
-apt autoremove -y
-apt install -y zsh
-apt install -y autojump
-apt install -y fonts-powerline
-apt install -y tmux
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install -y zsh
+sudo apt install -y autojump
+sudo apt install -y fonts-powerline
+sudo apt install -y tmux
 
+# github CLI 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
 
 # export proxy
-echo 'export host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")' >> /home/cuih/.bashrc
-echo 'export HTTP_PROXY=http://$host_ip:7890' >> /home/cuih/.bashrc
-echo 'export http_proxy=http://$host_ip:7890' >> /home/cuih/.bashrc
-echo 'export HTTPS_PROXY=http://$host_ip:7890' >> /home/cuih/.bashrc
-echo 'export https_proxy=http://$host_ip:7890' >> /home/cuih/.bashrc
+echo 'export host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")' >> ~/.bashrc
+echo 'export HTTP_PROXY=http://$host_ip:7890' >> ~/.bashrc
+echo 'export http_proxy=http://$host_ip:7890' >> ~/.bashrc
+echo 'export HTTPS_PROXY=http://$host_ip:7890' >> ~/.bashrc
+echo 'export https_proxy=http://$host_ip:7890' >> ~/.bashrc
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
