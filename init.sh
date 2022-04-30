9ddc0399b2be12641405b20e63a5188f432e32d5
hangcui <767948418@qq.com>
2022-04-30 10:48:30 +0800

    uncomment

--- init.sh
+++ init.sh
@@ -1,29 +1,31 @@
 # source list
-# mv /etc/apt/sources.list /etc/apt/sources.list.bak
-#cp ./aliyun_focal_sources.list  /etc/apt
-#mv /etc/apt/aliyun_focal_sources.list /etc/apt/sources.list
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cp ./aliyun_focal_sources.list  /etc/apt
+mv /etc/apt/aliyun_focal_sources.list /etc/apt/sources.list
 
 # wsl2 nameserver tmp
-#echo 'nameserver:8.8.8.8' >> /etc/reslov.conf
-#echo 'nameserver:8.8.4.4' >> /etc/reslov.conf
+echo 'nameserver:8.8.8.8' >> /etc/reslov.conf
+echo 'nameserver:8.8.4.4' >> /etc/reslov.conf
 
 # apt
-#apt update
-#apt upgrade -y
-#apt autoremove -y
-#apt install -y zsh
-#apt install -y autojump
-#apt install -y fonts-powerline
+apt update
+apt upgrade -y
+apt autoremove -y
+apt install -y zsh
+apt install -y autojump
+apt install -y fonts-powerline
+apt install -y tmux
+
 
 # export proxy
-#echo 'export host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")' >> /home/cuih/.bashrc
-#echo 'export HTTP_PROXY=http://$host_ip:7890' >> /home/cuih/.bashrc
-#echo 'export http_proxy=http://$host_ip:7890' >> /home/cuih/.bashrc
-#echo 'export HTTPS_PROXY=http://$host_ip:7890' >> /home/cuih/.bashrc
-#echo 'export https_proxy=http://$host_ip:7890' >> /home/cuih/.bashrc
+echo 'export host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")' >> /home/cuih/.bashrc
+echo 'export HTTP_PROXY=http://$host_ip:7890' >> /home/cuih/.bashrc
+echo 'export http_proxy=http://$host_ip:7890' >> /home/cuih/.bashrc
+echo 'export HTTPS_PROXY=http://$host_ip:7890' >> /home/cuih/.bashrc
+echo 'export https_proxy=http://$host_ip:7890' >> /home/cuih/.bashrc
 
 # oh my zsh
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#echo 'prompt_context(){}' >> ~/.zshrc
-#source ~/.zshrc
+echo 'prompt_context(){}' >> ~/.zshrc
+source ~/.zshrc
